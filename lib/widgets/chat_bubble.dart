eeec5d2d2f2392fd5839c2b3d5986256238d2f1d
Divine Quijano <quijanodivine2@gmail.com>
2025-05-29 00:07:14 +0800

    Improvig UI with Box Constraints

--- lib/widgets/chat_bubble.dart
+++ lib/widgets/chat_bubble.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 import '../models/ chat_message_entity.dart';
 
 class ChatBubble extends StatelessWidget{
@@ -12,6 +13,7 @@ class ChatBubble extends StatelessWidget{
     return Align(
       alignment: alignment,
       child: Container(
+        constraints: BoxConstraints(maxWidth: MediaQuery.of(context).size.width * 0.5),
         padding: EdgeInsets.all(24),
         child: Column(
           mainAxisSize: MainAxisSize.min,
