79ac22fb7e3db65e22b4c8eaee1692354ae18acc
Divine Quijano <quijanodivine2@gmail.com>
2025-05-29 15:59:17 +0800

    Static Initializers

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,9 @@ import 'package:flutter/material.dart';
 import 'package:new_chat_app/services/auth_service.dart';
 import 'package:provider/provider.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await AuthService.init();
   runApp(Provider(
       create: (BuildContext context) => AuthService(),
       child: ChatApp()
--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -1,25 +1,25 @@
 import 'package:shared_preferences/shared_preferences.dart';
 
 class AuthService {
+  static late SharedPreferences _prefs;
 
-  final Future<SharedPreferences> _prefs = SharedPreferences.getInstance();
+  static Future<void> init() async {
+    _prefs = await SharedPreferences.getInstance();
+  }
 
   Future<void> loginUser(String userName) async {
     try {
-      SharedPreferences sharedPrefs = await _prefs;
-      sharedPrefs.setString('userName', userName);
-    }catch(e){
+      await _prefs.setString('userName', userName);
+    } catch (e) {
       print(e);
     }
   }
 
-  void logoutUser() async {
-    SharedPreferences sharedPrefs = await _prefs;
-    sharedPrefs.clear();
+  void logoutUser() {
+    _prefs.clear();
   }
 
-  Future<String?> getUserName() async {
-    SharedPreferences sharedPrefs = await _prefs;
-    return sharedPrefs.getString('userName') ?? 'DefaultValue';
+  String? getUserName() {
+    return _prefs.getString('userName') ?? 'DefaultValue';
   }
 }
