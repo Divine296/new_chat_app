d6f1a19d58df2b30a9d4037ca82b67ceb2a717ba
Divine Quijano <quijanodivine2@gmail.com>
2025-05-29 15:44:12 +0800

    Caching Data

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -1,9 +1,25 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
 class AuthService {
-  void loginUser() {}
 
-  void logoutUser() {}
+  final Future<SharedPreferences> _prefs = SharedPreferences.getInstance();
 
-  String getUserName() {
-    return "Divine";
+  Future<void> loginUser(String userName) async {
+    try {
+      SharedPreferences sharedPrefs = await _prefs;
+      sharedPrefs.setString('userName', userName);
+    }catch(e){
+      print(e);
+    }
+  }
+
+  void logoutUser() async {
+    SharedPreferences sharedPrefs = await _prefs;
+    sharedPrefs.clear();
+  }
+
+  Future<String?> getUserName() async {
+    SharedPreferences sharedPrefs = await _prefs;
+    return sharedPrefs.getString('userName') ?? 'DefaultValue';
   }
 }
